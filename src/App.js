d7d9f6c5331f51746188bdb153e610447705505b
AleksandraLiadova <56653235+AleksandraLiadova@users.noreply.github.com>
2021-02-16 18:09:49 +0300

    in process

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import 'normalize.css';
+import "normalize.css";
 import "./App.scss";
 import React from "react";
 
@@ -15,20 +15,45 @@ class App extends React.Component {
             <div className="header-logo">
               <div className="logo"></div>
               <div className="company-name">
-              <span className="bla">BLA BLA</span>
-              <span className="subtitle">One Page Flat Template</span>
+                <span className="bla">BLA BLA</span>
+                <span className="subtitle">One Page Flat Template</span>
               </div>
             </div>
             <div className="navbar">
               <ul className="navbar-items">
-                <li><a href="#" className="navbar-item-link">home</a></li>
-                <li><a href="#" className="navbar-item-link">portfolio</a></li>
-                <li><a href="#" className="navbar-item-link">about</a></li>
-                <li><a href="#" className="navbar-item-link">contact</a></li>
+                <li>
+                  <a href="#" className="navbar-item-link">
+                    home
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="navbar-item-link">
+                    portfolio
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="navbar-item-link">
+                    about
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="navbar-item-link">
+                    contact
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
-          <div className="brand"></div>
+          <div className="brand">
+            <div className="brand-items">
+              <div className="brand-tagline">
+                We Build <span className="weight-font">Brand</span>
+              </div>
+              <div className="lorem">Lorem</div>
+              <div className="link-learn-more">learn more</div>
+              <div className="bottom-button"></div>
+            </div>
+          </div>
           <div className="portfolio"></div>
           <div className="about-us"></div>
           <div className="contacts"></div>
